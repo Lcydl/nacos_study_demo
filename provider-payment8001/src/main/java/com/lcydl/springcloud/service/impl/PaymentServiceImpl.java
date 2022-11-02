9d617ee0971434c293b3eaf563f070e70ce35d4c
Lcydl <1843336344@qq.com>
2022-11-02 16:46:50 +0800

    消费者consumer-order80

--- provider-payment8001/src/main/java/com/lcydl/springcloud/service/impl/PaymentServiceImpl.java
+++ provider-payment8001/src/main/java/com/lcydl/springcloud/service/impl/PaymentServiceImpl.java
@@ -3,9 +3,11 @@ package com.lcydl.springcloud.service.impl;
 import com.lcydl.springcloud.dao.PaymentDao;
 import com.lcydl.springcloud.entities.Payment;
 import com.lcydl.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
     @Resource
