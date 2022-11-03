0040e81a78d2044c81a327262dea56e7411accdc
Lcydl <1843336344@qq.com>
2022-11-03 18:20:23 +0800

    引入Eureka

--- consumer-order80/src/main/java/com/lcydl/springcloud/config/ApplicationContextConfig.java
+++ consumer-order80/src/main/java/com/lcydl/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.lcydl.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
 
+    /**
+     * @LoadBalanced 负载均衡，本机名有下划线的情况下会报错
+     * @return
+     */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
