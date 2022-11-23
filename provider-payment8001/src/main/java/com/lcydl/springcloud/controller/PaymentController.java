e1d2f3790e364d2fae23727c17f09b349eabde90
Lcydl <1843336344@qq.com>
2022-11-23 18:11:04 +0800

    eureka server配置

--- provider-payment8001/src/main/java/com/lcydl/springcloud/controller/PaymentController.java
+++ provider-payment8001/src/main/java/com/lcydl/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.lcydl.springcloud.entities.Payment;
 import com.lcydl.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,6 +18,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private  String serverPort;
@@ -39,6 +45,19 @@ public class PaymentController {
         } else {
             return new CommonResult<>(500, "没有对应记录，查询ID：" + id, null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object main() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element: " + element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
