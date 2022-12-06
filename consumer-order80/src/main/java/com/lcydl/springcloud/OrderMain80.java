2f8c3f05c3947c7acf05124199f2b18bfecfaee6
Lcydl <1843336344@qq.com>
2022-12-06 18:04:58 +0800

    学习ribbon

--- consumer-order80/src/main/java/com/lcydl/springcloud/OrderMain80.java
+++ consumer-order80/src/main/java/com/lcydl/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.lcydl.springcloud;
 
+import com.lcydl.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args) {
