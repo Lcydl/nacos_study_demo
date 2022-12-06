6136a3f83f75c522e2a3bdea0d5e90dcb61a669b
Lcydl <1843336344@qq.com>
2022-12-06 16:52:04 +0800

    学习consul

---
+++ consumerconsul-order80/src/main/java/com/lcydl/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.lcydl.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+
+    /**
+     * @LoadBalanced 负载均衡，本机名有下划线的情况下会报错
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
