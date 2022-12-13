2186627b2a13b229884f0a2826fb312065eed7e7
Lcydl <1843336344@qq.com>
2022-12-13 17:53:15 +0800

    OpenFeign

---
+++ consumer-feign-order80/src/main/java/com/lcydl/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,20 @@
+package com.lcydl.springcloud.service;
+
+import com.lcydl.springcloud.entities.CommonResult;
+import com.lcydl.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    String paymentFeignTimeout();
+
+}
--- consumer-order80/src/main/java/com/lcydl/springcloud/config/ApplicationContextConfig.java
+++ consumer-order80/src/main/java/com/lcydl/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,5 @@
 package com.lcydl.springcloud.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +12,7 @@ public class ApplicationContextConfig {
      * @return
      */
     @Bean
-    @LoadBalanced
+    // @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
