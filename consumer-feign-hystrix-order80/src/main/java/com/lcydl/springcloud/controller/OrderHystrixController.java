5e8a81360cc49653ceb8b91317793546a3db27c9
Lcydl <1843336344@qq.com>
2022-12-15 16:29:44 +0800

    Hystrix服务降级

--- consumer-feign-hystrix-order80/src/main/java/com/lcydl/springcloud/controller/OrderHystrixController.java
+++ consumer-feign-hystrix-order80/src/main/java/com/lcydl/springcloud/controller/OrderHystrixController.java
@@ -1,7 +1,8 @@
 package com.lcydl.springcloud.controller;
 
 import com.lcydl.springcloud.service.PaymentHystrixService;
-import feign.Response;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
 
     @Resource
@@ -23,10 +25,21 @@ public class OrderHystrixController {
     };
 
     @GetMapping(value = "/consumer/payment/hystrix/timeout/{id}")
+//    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     };
 
+    public String paymentInfoTimeOutFallbackMethod(Integer id) {
+        return "我是消费者80，对方支付系统繁忙，请10秒钟后再试或者检查自己😣😥";
+    }
+
+    public String payment_Global_FallbackMethod() {
+        return "全局异常";
+    }
 
 }
